6b9c539c2493d19f8f9fd5fc913fe86d304ed30e
robMolloy <robmolloy@hotmail.co.uk>
2021-12-03 18:30:45 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const useNameSpacedStore = (storeName) => {
+  const store = useStore();
+
+  return {
+    state: (name) => store.state[`${storeName}/${name}`],
+    getters: (name) => store.getters[`${storeName}/${name}`],
+    dispatch: (name, payload) => store.dispatch(`${storeName}/${name}`, payload),
+    commit: (name, payload) => store.commit(`${storeName}/${name}`, payload),
+  };
+};
+
+module.exports = {useNameSpacedStore}

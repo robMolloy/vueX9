a0c7d0904741fed2ebaae0b3b40ad8af6799ddf7
robMolloy <robmolloy@hotmail.co.uk>
2021-12-04 05:42:31 +0000

    allow array options

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ export const useNameSpacedStore = (storeName) => {
   const store = useStore();
 
   return {
-    state: (name) => store.state[`${storeName}/${name}`],
+    state: (name) => {
+      const names = Array.isArray(name) ? name : [name]
+      const states = {}
+      names.forEach((x)=> states[x] = store.states[`${storeName}/${x}`])
+      return Array.isArray(name) ? states : states[name]
+    },
     getters: (name) => {
       const names = Array.isArray(name) ? name : [name]
       const getters = {}
       names.forEach((x)=> getters[x] = store.getters[`${storeName}/${x}`])
       return Array.isArray(name) ? getters : getters[name]
     },
-    dispatch: (name, payload) => store.dispatch(`${storeName}/${name}`, payload),
-    commit: (name, payload) => store.commit(`${storeName}/${name}`, payload),
+    dispatch: (name) => {
+      const names = Array.isArray(name) ? name : [name]
+      const dispatches = {}
+      names.forEach((x)=> dispatches[x] = store.dispatch.bind(this, `${storeName}/${x}`))
+      return Array.isArray(name) ? dispatches : dispatches[name]
+    },
+    commit: (name) => {
+      const names = Array.isArray(name) ? name : [name]
+      const commits = {}
+      names.forEach((x)=> commits[x] = store.commit.bind(this, `${storeName}/${x}`))
+      return Array.isArray(name) ? commits : commits[name]
+    },
   };
 };

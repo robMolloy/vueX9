24424cc2f24f01877042aed2108a973769dd0ace
robMolloy <robmolloy@hotmail.co.uk>
2021-12-04 06:11:10 +0000

    bugfix make reactive

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ export const useNameSpacedStore = (storeName) => {
     getters: (name) => {
       const names = Array.isArray(name) ? name : [name]
       const getters = {}
-      names.forEach((x)=> computed(() => getters[x] = store.getters[`${storeName}/${x}`]))
+      names.forEach((x)=> getters[x] = computed(() => store.getters[`${storeName}/${x}`]))
       return Array.isArray(name) ? getters : getters[name]
     },
     dispatch: (name) => {
